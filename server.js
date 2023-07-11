3492541a51e20bfe198be749263bc69202a5e804
SamanthaJoy13 <tavaresjsamantha52@icloud.com>
2023-07-11 14:32:26 -0700

    completed routes in server

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const notes = require('./db/db.json');
 
-
+const PORT = process.env.PORT || 3001;
 const app = express();
-const PORT = 3001;
 
+
+app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join('client')));
+app.use(express.urlencoded({ extended: true }));
 
 
-app.get('/public', (req, res) => 
-res.sendFile(path.join(__dirname, 'client', 'index.html'))
-);
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+  });
 
-app.get('/notes', (req, res) =>
-res.sendFile(path.join(__dirname, 'client', 'notes.html'))
-);
-
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
 });
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body); 
+    notes.push((req.body));
+    fs.writeFile('./db/db.json', JSON.stringify(notes, null, 2) , function (err) {
+        if (err) throw err;
+      });
+    res.json(req.body);
+});
+
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
