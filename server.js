287610ed6580a80a650e5caf1eeb358301be9b21
SamanthaJoy13 <tavaresjsamantha52@icloud.com>
2023-07-11 13:59:24 -0700

    added tasks to db.json

--- server.js
+++ server.js
@@ -4,8 +4,21 @@ const path = require('path');
 
 
 const app = express();
+const PORT = 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join('client')));
+
+
+app.get('/public', (req, res) => 
+res.sendFile(path.join(__dirname, 'client', 'index.html'))
+);
+
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, 'client', 'notes.html'))
+);
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
